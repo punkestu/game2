fdf93309da9078ff987a62b42ce0f101379d187d
punkestu <pangestubima89@gmail.com>
2022-05-21 17:57:18 +0700

    minor

--- js/socket.js
+++ js/socket.js
@@ -1,16 +1,16 @@
 class Socket {
   constructor(url = "") {
-    this.socket = new WebSocket(url);
+//     this.socket = new WebSocket(url);
   }
   onerror() {}
   onmessage() {}
   onopen() {}
   onclose() {}
   listen() {
-    this.socket.addEventListener("open", this.onopen);
-    this.socket.addEventListener("close", this.onclose);
-    this.socket.addEventListener("message", this.onmessage);
-    this.socket.addEventListener("error", this.onerror);
+//     this.socket.addEventListener("open", this.onopen);
+//     this.socket.addEventListener("close", this.onclose);
+//     this.socket.addEventListener("message", this.onmessage);
+//     this.socket.addEventListener("error", this.onerror);
   }
 }
 
